0f05172f58fb4d48c82860594626b58d9fd4bff5
Enzo Viola <marquesenzo@hotmail.com>
2025-10-23 15:38:35 -0300

    Firebase mais seguro

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Este arquivo NÃO deve ser enviado para o GitHub.
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY_REAL_AQUI",
+  authDomain: "cartoes-palhacos-app.firebaseapp.com",
+  projectId: "cartoes-palhacos-app",
+  storageBucket: "cartoes-palhacos-app.firebasestorage.app",
+  messagingSenderId: "455690604804",
+  appId: "1:455690604804:web:0b5a909a5ee2dec9e50051",
+  measurementId: "G-FC6MXKE9XC"
+};
